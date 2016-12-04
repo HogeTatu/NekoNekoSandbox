677952e5dd5816af5a60366de18ebd72f67c641f
HogeTatu <hararyo.tae@gmail.com>
2016-12-05 00:53:38 +0900

    動的テクスチャ生成

---
+++ Plugins/NekoNekoGeneral/Source/NekoNekoGeneral/Private/Texture/TextureBuildComponent.cpp
@@ -0,0 +1,57 @@
+﻿#include "NekoNekoGeneralPrivatePCH.h"
+#include "TextureBuildComponent.h"
+
+UTextureBuildComponent::UTextureBuildComponent(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+	, Texture(nullptr)
+{
+}
+
+bool UTextureBuildComponent::UpdateTexture(const TArray<uint8>& InTextureData, int32 InSizeX, int32 InSizeY, TEnumAsByte<EPixelFormat> InFormat)
+{
+	ETextureSourceFormat TextureFormat = TSF_Invalid;
+	TextureCompressionSettings CompressionSettings = TC_Default;
+	switch (InFormat)
+	{
+	case PF_B8G8R8A8:
+		TextureFormat = TSF_BGRA8;
+		break;
+	case PF_FloatRGBA:
+		TextureFormat = TSF_RGBA16F;
+		CompressionSettings = TC_HDR;
+		break;
+	}
+
+	// 必要ならテクスチャを作りなおす
+	if (!Texture
+		|| Texture->GetSizeX() != InSizeX
+		|| Texture->GetSizeY() != InSizeY
+		|| Texture->GetPixelFormat() != InFormat)
+	{
+		Texture = UTexture2D::CreateTransient(InSizeX, InSizeY, InFormat);
+	}
+
+	FTexture2DMipMap& Mip = Texture->PlatformData->Mips[0];
+	uint32* TextureData = (uint32*)Mip.BulkData.Lock(LOCK_READ_WRITE);
+	const int32 TextureDataSize = Mip.BulkData.GetBulkDataSize();
+
+	if (TextureDataSize != InTextureData.Num())
+	{
+		UE_LOG(LogTemp, Error, TEXT("TextureDataSize is invalid. TextureDataSize: %d, InTextureDataSize: %d"), TextureDataSize, InTextureData.Num());
+		Mip.BulkData.Unlock();
+		return false;
+	}
+
+	//　データコピー
+	FMemory::Memcpy(TextureData, InTextureData.GetData(), TextureDataSize);
+
+	Mip.BulkData.Unlock();
+	Texture->UpdateResource();
+
+	return true;
+}
+
+UTexture2D* UTextureBuildComponent::GetTexture() const
+{
+	return Texture;
+}
---
+++ Plugins/NekoNekoGeneral/Source/NekoNekoGeneral/Public/Texture/TextureBuildComponent.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "Components/ActorComponent.h"
+#include "TextureBuildComponent.generated.h"
+
+/** テクスチャ作成コンポーネント */
+UCLASS(meta=(BlueprintSpawnableComponent), hideCategories=(Mobility))
+class NEKONEKOGENERAL_API UTextureBuildComponent : public UActorComponent
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+	/**
+	 * テクスチャ更新
+	 * @param InTextureData テクスチャデータ
+	 * @param InSizeX サイズX
+	 * @param InSizeY サイズY
+	 * @param InFormat ピクセルフォーマット
+	 * @return 更新が完了したらtrue
+	 */
+	UFUNCTION(BlueprintCallable, Category="Texture")
+	bool UpdateTexture(const TArray<uint8>& InTextureData, int32 InSizeX, int32 InSizeY, TEnumAsByte<EPixelFormat> InFormat);
+
+	/**
+	 * テクスチャ取得
+	 * @return テクスチャ
+	 */
+	UFUNCTION(BlueprintPure, Category="Texture")
+	UTexture2D* GetTexture() const;
+
+private:
+	/** テクスチャ */
+	UPROPERTY()
+	UTexture2D* Texture;
+};
